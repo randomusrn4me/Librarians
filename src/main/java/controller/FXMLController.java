891a09663995fea5cb0e9ca6017a7898b1b96e7d
Beren <berenyimobil@gmail.com>
2020-03-29 17:58:03 +0200

    asd

---
+++ src/main/java/controller/FXMLController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.util.Random;
+
+
+public class FXMLController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}

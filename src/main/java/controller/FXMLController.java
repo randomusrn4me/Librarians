6ab84227aed8af2ff83dbde631b713e6c8b9b2c7
Pat <patrik1katai@gmail.com>
2020-04-01 13:33:30 +0200

    minor gui changes

--- src/main/java/controller/FXMLController.java
+++ src/main/java/controller/FXMLController.java
@@ -1,35 +1,86 @@
 package controller;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import java.util.Random;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import com.jfoenix.controls.JFXButton;
+import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXTextField;
+import javafx.stage.Stage;
 
 public class FXMLController implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
 
     @FXML
     private JFXButton addBook;
 
     @FXML
+    private JFXTextField title;
+
+    @FXML
+    private JFXTextField author;
+
+    @FXML
+    private JFXTextField year;
+
+    @FXML
+    private JFXTextField publisher;
+
+    @FXML
     private JFXButton cancel;
+
 
     @FXML
     void handleAddBookButtonPushed() {
-        System.out.println("AddBook button pushed!!");
+        String bookTitle = title.getText();
+        String bookAuthor = author.getText();
+        String bookYear = year.getText();
+        String bookPublisher = publisher.getText();
+
+        if(bookTitle.isEmpty() || bookAuthor.isEmpty() || bookYear.isEmpty() || bookPublisher.isEmpty()){
+            Alert emptyAlert = new Alert(Alert.AlertType.ERROR);
+            emptyAlert.setHeaderText(null);
+            emptyAlert.setContentText("Please fill out all fields.");
+            emptyAlert.showAndWait();
+            //return;
+        }
+        else if(!Character.isDigit(bookYear.charAt(0))){
+            Alert numberAlert = new Alert(Alert.AlertType.ERROR);
+            numberAlert.setHeaderText("Incorrect Year");
+            numberAlert.setContentText("Please enter a valid year.");
+            numberAlert.showAndWait();
+        }
+        else if(Integer.parseInt(bookYear) > Calendar.getInstance().get(Calendar.YEAR)){
+            Alert numberAlert = new Alert(Alert.AlertType.ERROR);
+            numberAlert.setHeaderText("Incorrect Year");
+            numberAlert.setContentText("Please enter a valid year.");
+            numberAlert.showAndWait();
+        }
+        else{
+            Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+            successAlert.setHeaderText(null);
+            successAlert.setContentText("The book has been added successfully.");
+            successAlert.showAndWait();
+        }
+
     }
 
     @FXML
     void handleCancelButtonPushed() {
-        System.out.println("Cancel button pushed!!");
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }

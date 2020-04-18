fa578f6b53d2523cb36fdfc999586dc4fe12a2ab
Beren <berenyimobil@gmail.com>
2020-04-18 10:13:34 +0200

    Login design adjustment, reacts to pressing ENTER

--- src/main/java/ui/login/LoginController.java
+++ src/main/java/ui/login/LoginController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -26,6 +27,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -57,7 +60,7 @@ public class LoginController implements Initializable {
     private Text statusText;
 
     @FXML
-    void loginButtonPushed() throws InterruptedException {
+    void loginButtonPushed() {
         String username = usernameBox.getText();
         String password = hashing(passwordBox.getText());
         statusText.setStyle("-fx-font-weight:bold");
@@ -70,33 +73,24 @@ public class LoginController implements Initializable {
         if(loginFileAccess.getMapOfUsers().containsKey(username) && loginFileAccess.getMapOfUsers().get(username)[0].equals(password)){
             statusText.setText("You are a valid user");
             statusText.setFill(Color.MEDIUMSEAGREEN);
-
             if(loginFileAccess.getMapOfUsers().get(username)[1].equals("user")){
                 System.out.println("A user (\"" + username +"\") has logged in!");
                 closeStage();
                 userpanelLoader();
-                //Thread.sleep(4000);
-                //LoginMain.close();
-                // Closing login window
-                // Opening user privilege window
             } else if(loginFileAccess.getMapOfUsers().get(username)[1].equals("admin")){
                 System.out.println("An admin (\"" + username +"\") has logged in!");
                 closeStage();
                 mainframeLoader();
-                //Thread.sleep(4000);
-                //Application.launch(MainframeMain.class);
-                //LoginMain.close();
-                // Closing login window
-                // Opening admin privilege window
             }
         } else {
-
             statusText.setText("Invalid user");
             statusText.setFill(Color.RED);
             System.out.println("An invalid (\"" + username + "\") user has attempted login!");
         }
+    }
 
-
+    public void cancelButtonPushed() {
+        closeStage();
     }
 
     public static String hashing(String pw){
@@ -138,7 +132,6 @@ public class LoginController implements Initializable {
             stage.setTitle("Personal Library Manager");
             stage.setScene(new Scene(parent));
             stage.show();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -149,10 +142,26 @@ public class LoginController implements Initializable {
         statusText.setText("Please sign in to start using the software!");
         loginFileAccess.addUser("admin2", hashing("administrator"), "admin");
         loginFileAccess.addUser("test", hashing("testing"), "user");
+
+        passwordBox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER)  {
+                    loginButtonPushed();
+                }
+            }
+        });
+
+        usernameBox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER)  {
+                    loginButtonPushed();
+                }
+            }
+        });
     }
 
 
-    public void cancelButtonPushed() {
-        closeStage();
-    }
+
 }
